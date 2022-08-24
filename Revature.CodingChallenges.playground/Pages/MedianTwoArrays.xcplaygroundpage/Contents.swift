49e6fe7aeaf84f2030d984e797aa0a2caac6e1b6
Curtis Becker <ceb2941@gmail.com>
2022-08-24 16:00:20 -0400

    added new problems

--- Revature.CodingChallenges.playground/Pages/MedianTwoArrays.xcplaygroundpage/Contents.swift
+++ Revature.CodingChallenges.playground/Pages/MedianTwoArrays.xcplaygroundpage/Contents.swift
@@ -2,6 +2,25 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//given [1,2], [3,4]
+//output 2.5
+//
 
+func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
+    var arr = nums1 + nums2
+    arr = arr.sorted {$0 < $1}
+    if arr.count % 2 == 0{
+        var left : Double = Double(arr[(arr.count / 2) - 1])
+        var right : Double = Double(arr[arr.count / 2])
+        return (left + right) / 2
+    }
+    else{
+        return Double(arr[arr.count / 2])
+    }
+}
+
+let nums1 = [1, 2]
+let nums2 = [3, 4]
+
+print(findMedianSortedArrays(nums1, nums2))
 //: [Next](@next)
--- Revature.CodingChallenges.playground/Pages/MergeKLists.xcplaygroundpage/Contents.swift
+++ Revature.CodingChallenges.playground/Pages/MergeKLists.xcplaygroundpage/Contents.swift
@@ -2,6 +2,66 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//input [[1,4,5],[1,3,4],[2,6]]
+//output [1,1,2,3,4,4,5,6]
 
+class ListNode{
+    var val: Int
+    var next: ListNode?
+    
+    init(_ val: Int){
+        self.val = val
+        self.next = nil
+    }
+}
+
+func printList(_ head: ListNode?){
+    var current = head
+    while current != nil{
+        print(current!.val)
+        current = current?.next
+    }
+}
+
+func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
+    var arr : [Int] = []
+
+    for list in lists{
+        var currentList = list
+        while currentList != nil{
+            arr.append(currentList!.val)
+            currentList = currentList?.next
+        }
+    }
+    
+    if arr.count > 0{
+        arr = arr.sorted {$0 < $1}
+        let head : ListNode? = ListNode(arr[0])
+        var current = head
+
+        for i in 1..<arr.count{
+            let node : ListNode? = ListNode(arr[i])
+            current?.next = node
+            current = current?.next
+        }
+
+        return head
+    }
+    return nil
+}
+
+let head1 : ListNode? = ListNode(1)
+head1?.next = ListNode(4)
+head1?.next?.next = ListNode(5)
+
+let head2: ListNode? = ListNode(1)
+head2?.next = ListNode(3)
+head2?.next?.next = ListNode(4)
+
+let head3: ListNode? = ListNode(2)
+head3?.next = ListNode(6)
+
+var arrLists: [ListNode?] = [head1, head2, head3]
+var mergedSortedHead = mergeKLists(arrLists)
+printList(mergedSortedHead)
 //: [Next](@next)
